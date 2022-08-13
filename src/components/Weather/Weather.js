ceaf1cb6b36c831c747e56cb91be0eb9422ef83b
JFHarris <JFHarris1983@gmail.com>
2022-08-12 23:50:32 -0500

    Completed dynamic background color

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -35,14 +35,13 @@ function Weather() {
 
   const formatBackground = () => {
     if (!weather) return "from-cyan-700 to-blue-700"
-    const threshold = units === "imperial" ? 40 : 80
+    const threshold = units === "farenheit" ? 0 : 70
     if (weather.temp <= threshold) return "from-cyan-700 to-blue-700"
+    return "from-yellow-700 to-orange-700";
   }
 
   return (
-    <div className="mx-auto max-w-screen-md 
-    mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 
-    h-fit shadow-xl shadow-gray-400">
+    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
